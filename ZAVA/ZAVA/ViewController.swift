b768bee38c931128b06e2f6563f3818306600714
junha <goodjunha@gmail.com>
2022-12-03 20:43:23 +0900

    ❤️‍🔥:: first base Setting

---
+++ ZAVA/ZAVA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZAVA
+//
+//  Created by 박준하 on 2022/12/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
